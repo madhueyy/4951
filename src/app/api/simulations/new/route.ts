ab38531456f75a83f3693b918aeb1d3d80eb1218
Madhu Shrestha <madhu.shrestha666@gmail.com>
2026-03-08 18:38:02 +1100

    fixing route.ts for simulations

--- src/app/api/simulations/new/route.ts
+++ src/app/api/simulations/new/route.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import type { Session } from "next-auth";
 import clientPromise from "../../../lib/mongodb";
 
